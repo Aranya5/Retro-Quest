c82dc80b39555058a742676f316aa7fd9e9b1530
Aranya Basu <aranyabasu5@gmail.com>
2026-02-11 21:14:34 +0530

    Map loaded with player

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
   // SCREEN SETTINGS
@@ -15,21 +16,17 @@ public class GamePanel extends JPanel implements Runnable {
   final int scale = 3;
 
   public final int tileSize = originalTileSize * scale; // 48x48 tile
-  final int maxScreenCol = 16;
-  final int maxScreenRow = 12;
-  final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-  final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+  public final int maxScreenCol = 16;
+  public final int maxScreenRow = 12;
+  public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+  public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
   // FPS
   int FPS = 60;
-
+  TileManager tileM = new TileManager(this);
   KeyHandler keyH = new KeyHandler();
   Thread gameThread;
   Player player = new Player(this, keyH);
-
-  int playerX = 100;
-  int playerY = 100;
-  int playerSpeed = 4;
 
   public GamePanel() {
     this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -52,7 +49,7 @@ public class GamePanel extends JPanel implements Runnable {
     long lastTime = System.nanoTime();
     long currentTime;
     long timer = 0;
-    //int drawCount = 0;
+    // int drawCount = 0;
 
     while (gameThread != null) {
 
@@ -66,12 +63,12 @@ public class GamePanel extends JPanel implements Runnable {
         update();
         repaint();
         delta--;
-        //drawCount++;
+        // drawCount++;
       }
 
       if (timer >= 1000000000) {
-        //System.out.println("FPS: " + drawCount);
-        //drawCount = 0;
+        // System.out.println("FPS: " + drawCount);
+        // drawCount = 0;
         timer = 0;
       }
     }
@@ -87,6 +84,8 @@ public class GamePanel extends JPanel implements Runnable {
     super.paintComponent(g);
 
     Graphics2D g2 = (Graphics2D) g;
+
+    tileM.draw(g2);
 
     player.draw(g2);
 
