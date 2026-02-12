dc54fea74776d5159b93dde744c6f3770196f7e4
Aranya Basu <aranyabasu5@gmail.com>
2026-02-12 23:02:01 +0530

    Collision checking in progress

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -43,6 +43,7 @@ public class TileManager {
 
       tile[4] = new Tile();
       tile[4].image = ImageIO.read(getClass().getResourceAsStream("/tiles/tree.png"));
+      tile[4].collision = true;
 
       tile[5] = new Tile();
       tile[5].image = ImageIO.read(getClass().getResourceAsStream("/tiles/sand.png"));
